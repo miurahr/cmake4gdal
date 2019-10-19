c929d663d460c3bcf3d6a174baae1433586baf78
Hiroshi Miura <miurahr@linux.com>
2019-10-19 21:08:10 +0900

    CI: update
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- ci/azure-pipelines/osx-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-brew update
-brew outdated
-brew install freexl libxml2 libspatialite geos proj libpng openjpeg giflib jpeg szip postgresql postgis openssl poppler
-brew install ccache swig
