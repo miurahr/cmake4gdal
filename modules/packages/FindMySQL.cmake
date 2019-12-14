2b58ba5084db3e01b4b944e295494d0cbc1ada6b
Hiroshi Miura <miurahr@linux.com>
2019-12-14 22:28:22 +0900

    Improve FindMySQL
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- modules/packages/FindMySQL.cmake
+++ modules/packages/FindMySQL.cmake
@@ -14,21 +14,31 @@ find_path(MYSQL_INCLUDE_DIR
           NAMES mysql.h
           PATH_SUFFIXES mysql
           DOC "MySQL Client library includes")
+
+if( MYSQL_INCLUDE_DIR AND EXISTS "${MYSQL_INCLUDE_DIRS}/mysql_version.h" )
+	file( STRINGS "${MYSQL_INCLUDE_DIRS}/mysql_version.h"
+		MYSQL_VERSION_H REGEX "^#define[ \t]+MYSQL_SERVER_VERSION[ \t]+\"[^\"]+\".*$" )
+	string( REGEX REPLACE
+		"^.*MYSQL_SERVER_VERSION[ \t]+\"([^\"]+)\".*$" "\\1" MYSQL_VERSION_STRING
+		"${MYSQL_VERSION_H}" )
+endif()
+
 find_library(MYSQL_LIBRARY
-             NAMES mysqlclient mysqlclient_r)
-mark_as_advanced(MYSQL_LIBRARY MYSQL_INCLUDE_DIR)
+						 NAMES mysqlclient mysqlclient_r)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(MYSQL
                                   FOUND_VAR MYSQL_FOUND
-                                  REQUIRED_VARS MYSQL_LIBRARY MYSQL_INCLUDE_DIR)
+                                  REQUIRED_VARS MYSQL_LIBRARY MYSQL_INCLUDE_DIR
+																	VERSION_VAR MYSQL_VERSION_STRING)
 include(FeatureSummary)
 set_package_properties(MYSQL PROPERTIES
                        DESCRIPTION "MySQL Client library"
                        URL "https://dev.mysql.com/downloads/c-api/"
                        )
-if (MYSQL_FOUND)
-  set(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
-  set(MYSQL_INCLUDE_DIRS ${MYSQL_INCLUDE_DIRS})
-endif()
+
+mark_as_advanced(MYSQL_LIBRARY MYSQL_INCLUDE_DIR)
+
+set(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
+set(MYSQL_INCLUDE_DIRS ${MYSQL_INCLUDE_DIRS})
 
