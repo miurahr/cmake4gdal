06c4ea18eff4629c64f90b64661dda8937dcdd74
Hiroshi Miura <miurahr@linux.com>
2020-04-19 08:59:06 +0900

    Update default configuration examples
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- configurations/full_drivers.cmake
+++ configurations/full_drivers.cmake
@@ -1,28 +1,3 @@
-# ******************************************************************************
-# * Project:  CMake4GDAL
-# * Purpose:  CMake build scripts
-# * Author: Hiroshi Miura
-# ******************************************************************************
-# * Copyright (C) 2017,2018 Hiroshi Miura
-# *
-# * Permission is hereby granted, free of charge, to any person obtaining a
-# * copy of this software and associated documentation files (the "Software"),
-# * to deal in the Software without restriction, including without limitation
-# * the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# * and/or sell copies of the Software, and to permit persons to whom the
-# * Software is furnished to do so, subject to the following conditions:
-# *
-# * The above copyright notice and this permission notice shall be included
-# * in all copies or substantial portions of the Software.
-# *
-# * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# * DEALINGS IN THE SOFTWARE.
-# ******************************************************************************
 
 ## for debug
 #set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "")
@@ -99,8 +74,8 @@ set(GDAL_ENABLE_FRMT_OPENJPEG ON CACHE BOOL "")
 set(GDAL_ENABLE_FRMT_OZI ON CACHE BOOL "")
 set(GDAL_ENABLE_FRMT_PCIDSK ON CACHE BOOL "")
 set(GDAL_ENABLE_FRMT_PCRASTER ON CACHE BOOL "")
-set(GDAL_ENABLE_FRMT_POSTGISRASTER ON CACHE BOOL "")
 set(GDAL_ENABLE_FRMT_PDF ON CACHE BOOL "")
+set(GDAL_ENABLE_FRMT_POSTGISRASTER ON CACHE BOOL "")
 set(GDAL_ENABLE_FRMT_PDS ON CACHE BOOL "")
 set(GDAL_ENABLE_FRMT_PLMOSAIC ON CACHE BOOL "")
 set(GDAL_ENABLE_FRMT_PNG ON CACHE BOOL "")
