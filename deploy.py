6f011d8f0aaf0fdcd48dd586222cd1f06a228da9
Hiroshi Miura <miurahr@linux.com>
2020-04-25 15:40:02 +0900

    create hard link with deploy
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- deploy.py
+++ deploy.py
@@ -4,8 +4,8 @@ import argparse
 import os
 
 
-cur_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(cur_dir)
+cur_dir = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+parent_dir = os.path.abspath(os.path.dirname(cur_dir))
 listpath = os.path.join(cur_dir, 'cmakelists')
 gdalsrc_dir = os.path.join(parent_dir, 'gdal')
 autotest_dir = os.path.join(parent_dir, 'autotest')
@@ -30,23 +30,35 @@ def check_folders():
         print('There is no missing new driver for cmake build.')
 
 
+def remove_links():
+    """remove links of CMakeLists in each project folders"""
+    for cur, _, files in os.walk(parent_dir):
+        if 'CMakeLists.txt' in files:
+            os.unlink(os.path.join(cur, 'CMakeLists.txt'))
+
+
 def create_links():
     """create symlinks of CMakeLists in each project folders"""
     for cur, _, files in os.walk(listpath):
         if 'CMakeLists.txt' in files:
             target = os.path.join(parent_dir, os.path.relpath(cur, start=listpath))
-            os.symlink(os.path.relpath(os.path.join(cur, 'CMakeLists.txt'), target),
-                       os.path.join(target, 'CMakeLists.txt'))
+            target_file = os.path.join(target, 'CMakeLists.txt')
+            if os.path.exists(target_file):
+                os.unlink(target_file)
+            os.link(os.path.abspath(os.path.join(cur, 'CMakeLists.txt')), target_file)
 
 
 def main(arg=None):
     parser = argparse.ArgumentParser(prog='package.py', description='cmake for gdal packager',
                                      formatter_class=argparse.RawTextHelpFormatter, add_help=True)
     parser.add_argument("-t", "--test", action='store_true', help="Check cmakefile integrity.")
+    parser.add_argument("-r", "--remove", action='store_true', help="Remove all linked CMakeLists.txt")
     args = parser.parse_args(arg)
 
     if args.test:
         check_folders()
+    elif args.remove:
+        remove_links()
     else:
         create_links()
     return 0
